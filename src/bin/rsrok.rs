83070c3a18d728598c4ea3153c2842292cbef5a1
sxend <arimitsu.k+github@gmail.com>
2017-09-21 23:06:42 +0900

    refactor middleware

--- src/bin/rsrok.rs
+++ src/bin/rsrok.rs
@@ -2,7 +2,7 @@ extern crate clap;
 extern crate iron;
 extern crate router;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App, Arg, SubCommand};
 use iron::*;
 use iron::typemap;
 use iron::headers::*;
@@ -11,17 +11,24 @@ use router::Router;
 fn main() {
     let matches = App::new("rsrok")
         .version("0.1.0")
-        .arg(Arg::with_name("host")
-            .long("host")
-            .value_name("HOST")
-            .help("set expose host")
-            .takes_value(true))
-        .arg(Arg::with_name("src")
-            .long("src")
-            .value_name("SRC")
-            .help("set source server")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("host")
+                .long("host")
+                .value_name("HOST")
+                .help("set expose host")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("src")
+                .long("src")
+                .value_name("SRC")
+                .help("set source server")
+                .takes_value(true),
+        )
         .get_matches();
-    println!("host: {}, src: {}", matches.value_of("host").unwrap(), matches.value_of("src").unwrap());
-
+    println!(
+        "host: {}, src: {}",
+        matches.value_of("host").unwrap(),
+        matches.value_of("src").unwrap()
+    );
 }
--- src/bin/rsrokd.rs
+++ src/bin/rsrokd.rs
@@ -3,7 +3,7 @@ extern crate iron;
 extern crate router;
 mod string_error;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use iron::*;
 use iron::headers::*;
 use router::Router;
@@ -12,40 +12,59 @@ use string_error::StringError;
 fn main() {
     let matches = App::new("rsrokd")
         .version("0.1.0")
-        .arg(Arg::with_name("host")
-            .long("host")
-            .value_name("HOST")
-            .help("set allowed host")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("host")
+                .long("host")
+                .value_name("HOST")
+                .help("set allowed host")
+                .takes_value(true),
+        )
         .get_matches();
-    matches.value_of("host");
     println!("host: {}", matches.value_of("host").unwrap());
     let host = matches.value_of("host").unwrap();
+
     let mut router = Router::new();
-    router.get("/api/v1/join", join, "GET /api/v1/join");
+    router.any("/", dummy, "dummy");
+
     let mut chain = Chain::new(router);
+    let rsrokd_middleware = RsrokdMiddleware {
+        host: host.to_string(),
+    };
     chain.link_around(rsrokd_middleware);
     Iron::new(chain).http(host).unwrap();
 }
 
-fn join(req: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, ContentType::json().0, "join")))
-}
-fn rsrokd_middleware(handler: Box<Handler>) -> Box<Handler> {
-    let new_handler = move |req: &mut Request| -> IronResult<Response> {
-        match req.headers.clone().get::<Host>() {
-            Some(host) if host.hostname == "00.rsrokd.local" =>
-                Ok(Response::with((status::Ok, ContentType::json().0, host.clone().hostname))),
-            Some(host) if host.hostname == "rsrokd.local" => {
-                let req = req;
-                handler.handle(req)
-            }
-            _ => Err(IronError::new(
-                StringError("invalid request".to_string()),
-                status::BadRequest,
-            ))
-        }
-    };
-    Box::new(new_handler)
+fn dummy(req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::NotFound)))
 }
 
+struct RsrokdMiddleware {
+    host: String,
+}
+
+impl AroundMiddleware for RsrokdMiddleware {
+    fn around(self, handler: Box<Handler>) -> Box<Handler> {
+        let new_handler = move |req: &mut Request| -> IronResult<Response> {
+            match req.headers.clone().get::<Host>() {
+                Some(host) if self.is_root_host(host.hostname.to_owned(), host.port) => {
+                    handler.handle(req)
+                }
+                Some(host) => Ok(Response::with(
+                    (status::Ok, ContentType::json().0, host.clone().hostname),
+                )),
+                _ => Err(IronError::new(
+                    StringError("invalid request".to_string()),
+                    status::BadRequest,
+                )),
+            }
+        };
+        Box::new(new_handler)
+    }
+}
+
+impl RsrokdMiddleware {
+    fn is_root_host(&self, target_host: String, target_port: Option<u16>) -> bool {
+        let port = target_port.map_or("".to_owned(), |port| ":".to_string() + &port.to_string());
+        self.host == (target_host + &port)
+    }
+}
